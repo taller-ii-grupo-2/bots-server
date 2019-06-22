c12d89767b801087ef1a58eef51ab4cb22f49132
Matias Rozanec <rozanecm@gmail.com>
2019-06-22 10:34:32 -0300

    update port

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 8080
+// set the port of our application
+// // process.env.PORT lets the port be set by Heroku
+const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => res.send('Live modifiable now? Huh, amazing! Hello World! This time, from a NODE server! Now even from docker!!! wooooaa'))
 
